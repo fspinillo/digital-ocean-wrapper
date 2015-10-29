3e0ed0025e2fa04f97d0767fb3600c985db9d55d
Frank <frank@spinillo.net>
2015-10-29 10:57:51 -0400

    Fixes for domain records

--- do.py
+++ do.py
@@ -24,30 +24,33 @@ class User(object):
         return domains_list
 
     def get_domain(self, domain):
+        token = {'Authorization': 'Bearer ' + self.token + ''}
         if domain is None:
             return "Please supply a domain. Ex. example.com"
         else:
             domain_url = base_url + 'domains/' + domain + ''
-            domain_info = requests.get(domain_url, headers = header).json()
-            if domain_info.status_code == 200:
+            domain_info = requests.get(domain_url, headers = token).json()
+            if requests.get(domain_url, headers = token).status_code == 200:
                 return domain_info
             else:
                 return "Unable to find domain information for %s" % domain
 
-    def domain_records(self):
-        records_url = base_url + 'records'
-        records = requests.get(records_url, headers=header).json()
-        if records.status_code == 200:
+    def domain_records(self, domain):
+        token = {'Authorization': 'Bearer ' + self.token + ''}
+        records_url = base_url + 'domains/' + domain + '/records'
+        records = requests.get(records_url, headers = token).json()
+        if requests.get(records_url, headers = token).status_code == 200:
             return records
         else:
             return "Unable to fetch domain records. Check authorization token"
 
     def get_domain_record(self, id):
+        token = {'Authorization': 'Bearer ' + self.token + ''}
         if id is None:
             return "Please supply a domain record id."
         else:
-            records_url = base_url + 'records/' + id + ''
-            record = requests.get(records_url, headers=header).json()
+            records_url = base_url + 'domains/' + domain + '/records/' + id + ''
+            record = requests.get(records_url, headers = token).json()
             if record.status_code == 200:
                 return records
             else:

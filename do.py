ac9ec514ae2abdfbb166841c771d992a9ea43c93
Frank <frank@spinillo.net>
2015-11-02 17:17:00 -0500

    More updates
    Created get function to check status codes and return data if passed
    
    added more url end points to be initialized at launch
    
    can now call for list of droplets

--- do.py
+++ do.py
@@ -1,25 +1,37 @@
 import requests
 import json
 
-base_url = 'https://api.digitalocean.com/v2/'
+base_url = 'https://api.digitalocean.com/v2'
 header_type = "'Content-Type': 'application/json'"
+
+
+def get(url, header, params=None):
+    data = requests.get(url, headers = header)
+    if data.status_code == 200:
+        data = data.json()
+    else:
+        data = 'Unable to validate get request'
+
+    return data
+
 
 class User(object):
 
     def __init__(self, token):
         self.token = token
+        self._account = '{BASE}/account'.format(BASE=base_url)
+        self._domains = '{BASE}/domains/'.format(BASE=base_url)
+        self._droplets = '{BASE}/droplets'.format(BASE=base_url)
 
     def account(self):
         token = {'Authorization': 'Bearer ' + self.token + ''}
-        account_url = base_url + 'account'
-        account_info = requests.get(account_url, headers = token).json()
+        account_info = get(self._account, header=token)
 
         return account_info
 
     def domains(self):
-        domain_url = base_url + 'domains'
-        header = {'Authorization': 'Bearer ' + self.token + '', 'Content-Type': 'application/json'}
-        domains_list = requests.get(domain_url, headers = header).json()
+        token = {'Authorization': 'Bearer ' + self.token + ''}
+        domains_list = get(self._domains, header=token)
 
         return domains_list
 
@@ -28,30 +40,26 @@ class User(object):
         if domain is None:
             return "Please supply a domain. Ex. example.com"
         else:
-            domain_url = base_url + 'domains/' + domain + ''
-            domain_info = requests.get(domain_url, headers = token).json()
-            if requests.get(domain_url, headers = token).status_code == 200:
-                return domain_info
-            else:
-                return "Unable to find domain information for %s" % domain
+            domain_url = self._domains + domain
+            domain_info = get(domain_url, header=token)
+            return domain_info
 
     def domain_records(self, domain):
         token = {'Authorization': 'Bearer ' + self.token + ''}
-        records_url = base_url + 'domains/' + domain + '/records'
-        records = requests.get(records_url, headers = token).json()
-        if requests.get(records_url, headers = token).status_code == 200:
-            return records
-        else:
-            return "Unable to fetch domain records. Check authorization token"
+        records_url = self._domains + domain + '/records'
+        records = get(records_url, header=token)
+        return records
 
-    def get_domain_record(self, id):
+    def get_domain_record(self, domain, domain_id):
         token = {'Authorization': 'Bearer ' + self.token + ''}
-        if id is None:
+        if domain_id is None:
             return "Please supply a domain record id."
         else:
-            records_url = base_url + 'domains/' + domain + '/records/' + id + ''
-            record = requests.get(records_url, headers = token).json()
-            if record.status_code == 200:
-                return records
-            else:
-                return "Unable to find supplied domain record id"
+            records_url = self._domains + domain + '/records/{ID}'.format(ID=domain_id)
+            record = get(records_url, header=token)
+            return record
+
+    def droplets(self):
+        token = {'Authorization': 'Bearer ' + self.token + ''}
+        droplet_list = get(self._droplets, header=token)
+        return droplet_list

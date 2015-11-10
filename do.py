daf51b586abbb4a2273d4f8edf2576d90162676e
Frank <frank@spinillo.net>
2015-11-10 16:10:27 -0500

    added two more actions + post request

--- do.py
+++ do.py
@@ -1,12 +1,28 @@
 import requests
 import json
+from datetime import datetime
 
 base_url = 'https://api.digitalocean.com/v2'
 header_type = "'Content-Type': 'application/json'"
 
 
-def get(url, header, params=None):
-    data = requests.get(url, headers = header)
+def get(url, header, payload=None):
+    if payload = None:
+        data = requests.get(url, headers = header)
+    else:
+        data = requests.get(url, headers = header, params = payload)
+    if data.status_code == 200:
+        data = data.json()
+    else:
+        data = 'Unable to validate get request'
+
+    return data
+
+def post(url, header, payload=None):
+    if payload = None:
+        data = requests.get(url, headers = header)
+    else:
+        data = requests.get(url, headers = header, params = payload)
     if data.status_code == 200:
         data = data.json()
     else:
@@ -77,3 +93,42 @@ class Droplet(object):
 
     def __init__(self, token):
         self.token = token
+        self._droplets = '{BASE}/droplets/'.format(BASE=base_url)
+
+    def action(self, action=None, droplet_id=None):
+        if action is None:
+            return "Please enter an action: shutdown, reboot, etc"
+        elif droplet_id is None:
+            return "Please enter a droplet id"
+        elif action == 'reboot' or 'restart':
+            payload = {'type': 'reboot'}
+        elif action == 'power cycle':
+            payload = {'type': 'power_cycle'}
+        elif action == 'shutdown':
+            payload = {'type': 'shutdown'}
+        elif action == 'power off':
+            payload = {'type': 'power_off'}
+        elif action == 'power on':
+            payload = {'type': 'power_on'}
+
+        url = self._droplets + droplet_id
+        token = {'Authorization': 'Bearer ' + self.token + ''}
+        droplet_action = post(url, header=token, payload=payload)
+        return droplet_action
+
+    def snapshot(self, name=None, droplet_id=None):
+        if droplet_id is None:
+            return 'Please enter a droplet id'
+
+        if name is None:
+            now = datetime.now()
+            snapshot_name = "%s-%s-%s" % (now.year, now.strftime("%m"), now.strftime("%d"))
+            payload = {'type': 'snapshot', 'name': snapshot_name}
+        else:
+            snapshot_name = name
+            payload = {'type': 'snapshot', 'name': snapshot_name}
+
+        url = self._droplets + droplet_id
+        token = {'Authorization': 'Bearer ' + self.token + ''}
+        snapshot_action = post(url, header=token, payload=payload)
+        return snapshot_action
